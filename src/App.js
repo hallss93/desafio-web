f9fbc7468ab3d43841bd1889f9a7304ebc65537e
Hallison Melo <hallss93@hotmail.com>
2019-12-17 13:37:19 -0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, ButtonToolbar, Navbar, Container, Image } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar expand="lg">
+      <Container>
+        <Image src="https://fonts.gstatic.com/s/i/materialicons/menu/v1/24px.svg" className="menu-icon" />
+        <Navbar.Brand href="#">Github JavaPop</Navbar.Brand>
+      </Container>
+    </Navbar>
+
+
+    {/* Listagem */}
+    </>
   );
 }
 
